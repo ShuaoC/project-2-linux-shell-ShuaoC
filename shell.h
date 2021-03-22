c53e8861433f390db6a9b3e721f23dc2d795cca4
Felix Chen <felix19981228@gmail.com>
2021-03-21 21:35:09 -0400

    add new items to .h files

--- shell.h
+++ shell.h
@@ -3,12 +3,13 @@
 #define MAX_ARG_LENGTH 64
 #define MAX_NUM_OF_ARGUMENTS 64
 
-typedef struct StrCmd{
+typedef struct StrCmd
+{
     int iCmdID;
     int iCmdType;
-    char* sInputFile;
-    char* sOutputFile;
-    char** args;
+    char *sInputFile;
+    char *sOutputFile;
+    char **args;
     int isRedirInputFile;
     int isRedirOutputFile;
     int isBackgroundRunning;
@@ -19,3 +20,20 @@ void Prompt();
 void IM();
 int RunMyShell(StrCmdArray *cmdList);
 void BatchMode(char *filename);
+void sigintHandler(int sig_num);
+void getUsername(char *username, int size);
+void getHostname(char *hostname, int size);
+int GetArgsCount(char *const *args);
+void GetAbsPath(char *AbsolutePath, const char *CurPath);
+int GetArgsCount(char *const *args);
+void FreeCommandArray(StrCmdArray *cmdList);
+void ShowError();
+char *getCmdLine( int *isvalid, FILE* inputStream );
+char** LineToArray(char *line);
+void OrgCommands(StrCmdArray *cmdList);
+void SplitCommands(char**args, StrCmdArray *cmdList);
+int RunBuildInCmd(StrCmd *cmd);
+int RunCommands(StrCmdArray *cmdList);
+int RunACommand(StrCmd *cmd);
+int RunCommandsNotWait(StrCmdArray *cmdList);
+int RunCommandsPipe(StrCmdArray *cmdList);
